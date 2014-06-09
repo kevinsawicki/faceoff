72cbab6146c266f7fce59dbeb803cbba40c11769
Kevin Sawicki <kevinsawicki@gmail.com>
2014-06-09 08:35:34 -0700

    Add odds graph

--- graph.js
+++ graph.js
@@ -3,38 +3,38 @@ var width = 960 - margin.left - margin.right;
 var height = 480 - margin.top - margin.bottom;
 
 var data = [
-  {"country": "Algeria", "wins": 2, "losses": 5, "draws": 2, "best": 13, "class": "algeria"},
-  {"country": "Argentina", "wins": 37, "losses": 20, "draws": 13, "best": 1, "champions": 2, "class": "argentina"},
-  {"country": "Australia", "wins": 2, "losses": 5, "draws": 3, "best": 14, "class": "australia"},
-  {"country": "Belgium", "wins": 10, "losses": 17, "draws": 9, "best": 4, "class": "belgium"},
-  {"country": "Bosnia", "wins": 0, "losses": 0, "draws": 0, "best": 32, "class": "bosnia"},
-  {"country": "Brazil", "wins": 67, "losses": 15, "draws": 15, "best": 1, "champions": 5, "class": "brazil"},
-  {"country": "Cameroon", "wins": 4, "losses": 9, "draws": 7, "best": 8, "class": "cameroon"},
-  {"country": "Chile", "wins": 9, "losses": 14, "draws": 6, "best": 3, "class": "chile"},
-  {"country": "Columbia", "wins": 3, "losses": 8, "draws": 2, "best": 14, "class": "columbia"},
-  {"country": "Costa Rica", "wins": 3, "losses": 6, "draws": 1, "best": 13, "class": "costa-rica"},
-  {"country": "Croatia", "wins": 6, "losses": 5, "draws": 2, "best": 3, "class": "croatia"},
-  {"country": "Ecuador", "wins": 3, "losses": 0, "draws": 4, "best": 12, "class": "ecuador"},
-  {"country": "England", "wins": 26, "losses": 14, "draws": 19, "best": 1, "champions": 1, "class": "england"},
-  {"country": "France", "wins": 25, "losses": 18, "draws": 11, "best": 1, "champions": 1, "class": "france"},
-  {"country": "Germany", "wins": 60, "losses": 20, "draws": 19, "best": 1, "champions": 3, "class": "germany"},
-  {"country": "Ghana", "wins": 4, "losses": 3, "draws": 2, "best": 7, "class": "ghana"},
-  {"country": "Greece", "wins": 1, "losses": 5, "draws": 0, "best": 24, "class": "greece"},
-  {"country": "Honduras", "wins": 0, "losses": 3, "draws": 3, "best": 18, "class": "honduras"},
-  {"country": "Iran", "wins": 1, "losses": 6, "draws": 2, "best": 14, "class": "iran"},
-  {"country": "Italy", "wins": 44, "losses": 15, "draws": 21, "best": 1, "champions": 4, "class": "italy"},
-  {"country": "Ivory Coast", "wins": 2, "draws": 3, "losses": 1, "best": 17, "class": "ivory-coast"},
-  {"country": "Japan", "wins": 4, "losses": 7, "draws": 3, "best": 9, "class": "japan"},
-  {"country": "Korea", "wins": 5, "losses": 15, "draws": 8, "best": 4, "class": "korea"},
-  {"country": "Mexico", "wins": 12, "losses": 24, "draws": 13, "best": 6, "class": "mexico"},
-  {"country": "Netherlands", "wins": 22, "losses": 11, "draws": 10, "best": 2, "class": "netherlands"},
-  {"country": "Nigeria", "wins": 4, "losses": 8, "draws": 2, "best": 9, "class": "nigeria"},
-  {"country": "Portugal", "wins": 12, "losses": 8, "draws": 3, "best": 3, "class": "portgual"},
-  {"country": "Russia", "wins": 2, "losses": 4, "draws": 0, "best": 22, "class": "russia"},
-  {"country": "Spain", "wins": 28, "losses": 16, "draws": 12, "best": 1, "champions": 1, "class": "spain"},
-  {"country": "Switzerland", "wins": 9, "losses": 14, "draws": 6, "best": 6, "class": "switzerland"},
-  {"country": "Uruguay", "wins": 18, "losses": 17, "draws": 12, "best": 1, "champions": 2, "class": "uruguay"},
-  {"country": "United States", "wins": 7, "losses": 17, "draws": 5, "best": 3, "class": "united-states"}
+  {"country": "Algeria", "wins": 2, "losses": 5, "draws": 2, "best": 13, "odds": 1500, "class": "algeria"},
+  {"country": "Argentina", "wins": 37, "losses": 20, "draws": 13, "best": 1, "odds": 4, "champions": 2, "class": "argentina"},
+  {"country": "Australia", "wins": 2, "losses": 5, "draws": 3, "best": 14, "odds": 2500, "class": "australia"},
+  {"country": "Belgium", "wins": 10, "losses": 17, "draws": 9, "best": 4, "odds": 20, "class": "belgium"},
+  {"country": "Bosnia", "wins": 0, "losses": 0, "draws": 0, "best": 32, "odds": 150, "class": "bosnia"},
+  {"country": "Brazil", "wins": 67, "losses": 15, "draws": 15, "best": 1, "odds": 11/4, "champions": 5, "class": "brazil"},
+  {"country": "Cameroon", "wins": 4, "losses": 9, "draws": 7, "best": 8, "odds": 750, "class": "cameroon"},
+  {"country": "Chile", "wins": 9, "losses": 14, "draws": 6, "best": 3, "odds": 50, "class": "chile"},
+  {"country": "Columbia", "wins": 3, "losses": 8, "draws": 2, "best": 14, "odds": 40, "class": "columbia"},
+  {"country": "Costa Rica", "wins": 3, "losses": 6, "draws": 1, "best": 13, "odds": 2500, "class": "costa-rica"},
+  {"country": "Croatia", "wins": 6, "losses": 5, "draws": 2, "best": 3, "odds": 175, "class": "croatia"},
+  {"country": "Ecuador", "wins": 3, "losses": 0, "draws": 4, "best": 12, "odds": 125, "class": "ecuador"},
+  {"country": "England", "wins": 26, "losses": 14, "draws": 19, "best": 1, "odds": 25, "champions": 1, "class": "england"},
+  {"country": "France", "wins": 25, "losses": 18, "draws": 11, "best": 1, "odds": 22, "champions": 1, "class": "france"},
+  {"country": "Germany", "wins": 60, "losses": 20, "draws": 19, "best": 1, "odds": 6, "champions": 3, "class": "germany"},
+  {"country": "Ghana", "wins": 4, "losses": 3, "draws": 2, "best": 7, "odds": 200, "class": "ghana"},
+  {"country": "Greece", "wins": 1, "losses": 5, "draws": 0, "best": 24, "odds": 200, "class": "greece"},
+  {"country": "Honduras", "wins": 0, "losses": 3, "draws": 3, "best": 18, "odds": 1500, "class": "honduras"},
+  {"country": "Iran", "wins": 1, "losses": 6, "draws": 2, "best": 14, "odds": 2500, "class": "iran"},
+  {"country": "Italy", "wins": 44, "losses": 15, "draws": 21, "best": 1, "odds": 25, "champions": 4, "class": "italy"},
+  {"country": "Ivory Coast", "wins": 2, "draws": 3, "losses": 1, "best": 17, "odds": 150, "class": "ivory-coast"},
+  {"country": "Japan", "wins": 4, "losses": 7, "draws": 3, "best": 9, "odds": 150, "class": "japan"},
+  {"country": "Korea", "wins": 5, "losses": 15, "draws": 8, "best": 4, "odds": 250, "class": "korea"},
+  {"country": "Mexico", "wins": 12, "losses": 24, "draws": 13, "best": 6, "odds": 200, "class": "mexico"},
+  {"country": "Netherlands", "wins": 22, "losses": 11, "draws": 10, "best": 2, "odds": 28, "class": "netherlands"},
+  {"country": "Nigeria", "wins": 4, "losses": 8, "draws": 2, "best": 9, "odds": 300, "class": "nigeria"},
+  {"country": "Portugal", "wins": 12, "losses": 8, "draws": 3, "best": 3, "odds": 22, "class": "portgual"},
+  {"country": "Russia", "wins": 2, "losses": 4, "draws": 0, "best": 22, "odds": 80, "class": "russia"},
+  {"country": "Spain", "wins": 28, "losses": 16, "draws": 12, "best": 1, "odds": 6, "champions": 1, "class": "spain"},
+  {"country": "Switzerland", "wins": 9, "losses": 14, "draws": 6, "best": 6, "odds": 100, "class": "switzerland"},
+  {"country": "Uruguay", "wins": 18, "losses": 17, "draws": 12, "best": 1, "champions": 2, "odds": 28, "class": "uruguay"},
+  {"country": "United States", "wins": 7, "losses": 17, "draws": 5, "best": 3, "odds": 250, "class": "united-states"}
 ];
 
 var graphRecords = function() {
@@ -196,7 +196,7 @@ var graphFinishes = function() {
       .attr("height", function(d) { return height - y(33 - d.best); });
 
   var legend = svg.selectAll(".legend")
-      .data(["1st", "2nd", "3rd", "4th", "5-10", "10-20", "20-30"])
+      .data(["1st", "2nd", "3rd", "4th", "5th - 10th", "10th - 20th", "20th - 30th"])
     .enter().append("g")
       .attr("class", "legend")
       .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
@@ -231,5 +231,100 @@ var graphFinishes = function() {
       .text(function(d) { return d; });
 }
 
-graphRecords()
-graphFinishes()
+var graphOdds = function() {
+  var x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
+  var y = d3.scale.linear().range([height, 0]);
+
+  var xAxis = d3.svg.axis().scale(x).orient("bottom").tickFormat(function(country) {
+    var flag = country.toLowerCase().replace(/ /g, '-');
+    svg.append("image")
+      .attr("x", x(country))
+      .attr("y", height + 5)
+      .attr("width", x.rangeBand())
+      .attr("height", 18)
+      .attr("xlink:href", "./assets/flags/"+ flag + ".svg")
+      .attr("preserveAspectRatio", "none")
+      .append("title")
+        .text(country);
+    return '';
+  });
+
+  var yAxis = d3.svg.axis().scale(y).orient("right").ticks(10);
+
+  var svg = d3.select("svg.odds")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var odds = data.slice().sort(function(a, b) {
+    return a.odds - b.odds;
+  });
+
+  x.domain(odds.map(function(d) { return d.country; }));
+  y.domain([1, d3.max(odds, function(d) { return d.odds; })]);
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .attr("transform", "translate(" + width + ",0)")
+      .call(yAxis);
+
+  svg.selectAll(".odds")
+      .data(odds)
+    .enter().append("rect")
+      .attr("class", function(d) {
+        if (d.odds >= 2000)
+          return "bar odds-2000";
+        if (d.odds >= 1000)
+          return "bar odds-1000";
+        if (d.odds >= 100)
+          return "bar odds-100";
+        if (d.odds >= 25)
+          return "bar odds-25";
+        return "bars odds-1"
+      })
+      .attr("x", function(d) { return x(d.country); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.odds); })
+      .attr("height", function(d) { return height - y(d.odds); });
+
+  var legend = svg.selectAll(".legend")
+      .data([">= 2000 to 1", ">= 1000 to 1", ">= 100 to 1", ">= 25 to 1", "< 25 to 1"])
+    .enter().append("g")
+      .attr("class", "legend")
+      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+  legend.append("rect")
+      .attr("x", 0)
+      .attr("width", 18)
+      .attr("height", 18)
+      .attr("class", function(d, i) {
+        switch (i) {
+        case 0:
+          return "odds-2000";
+        case 1:
+          return "odds-1000";
+        case 2:
+          return "odds-100";
+        case 3:
+          return "odds-25";
+        case 4:
+          return "odds-1";
+        }
+      });
+
+  legend.append("text")
+      .attr("x", 20)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .text(function(d) { return d; });
+}
+
+graphRecords();
+graphFinishes();
+graphOdds();
